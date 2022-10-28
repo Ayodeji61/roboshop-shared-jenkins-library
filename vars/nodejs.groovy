494f8a4be50619fe9dde0ba9642f8b9fdf185aab
Ayodeji61 <alabidejimichael@gmail.com>
2022-10-28 16:01:31 -0600

    Updating roboshop-shared-jenkins-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,14 @@
 def call() {
+    env.APPTYPE = "nodejs"
     node {
-
-
+        try {
+            common.codeCheckout()
             common.codeQuality()
             common.codeChecks()
             common.artifacts()
+        } catch (Exception e) {
+            mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'dejispot@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'dejispot@gmail.com'
+        }
 
     }
 
